59748ccaf585cdb0c622e7dd7dfcab72bed91a12
Mariano <german.marianog@gmail.com>
2014-06-28 14:24:19 -0300

    Conexion a la DB, EF

--- AltaCancha/Models/IdentityModels.cs
+++ AltaCancha/Models/IdentityModels.cs
@@ -3,12 +3,21 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System;
 
 namespace AltaCancha.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public List<Booking> Bookings { get; set; }
+        public DateTime CreateDate { get; set; }
+        public  DateTime ModDate{ get; set; }
+        public bool isActive { get; set; }
+
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -20,6 +29,15 @@ namespace AltaCancha.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Court> Courts { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Amenity> Amenities { get; set; }
+        public DbSet<FloorType> FloorTypes { get; set; }
+        public DbSet<SizeType> Type { get; set; }
+        public DbSet<OpenTime> OpenTimes { get; set; }
+        public DbSet<State> State { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
