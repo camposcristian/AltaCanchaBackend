df31968ec98c40b722a7f9b65a8df20522da2ce0
Cristian Campos <camposcristian90@gmail.com>
2014-06-29 05:31:46 -0300

    BitPay integration

---
+++ AltaCancha/Models/PayModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AltaCancha.Models
+{
+    public class PayModel
+    {
+        public int BookId { get; set; }
+    }
+}
